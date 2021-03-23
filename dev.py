5742c1fd485d9b9cf81d80383f1d948594c9875b
Henry Free <hf360@cam.ac.uk>
2021-03-23 02:08:13 +0000

    Test of module structure / thermo

---
+++ dev.py
@@ -0,0 +1,8 @@
+import octopus as octo
+
+# For testing WIP code
+
+fuel_name = "isopropanol"
+
+fuel = octo.Fluid(fuel_name)
+print(fuel.chem.rho)
--- octopus/main.py
+++ octopus/main.py
@@ -1,9 +1,14 @@
+import thermo
+
 class Fluid:
     """The fluid class is used to implement any fluids passing through the injector."""
+
+    def __init__(self, name):
+        self.name = name
+        self.chem = thermo.chemical.Chemical(self.name)
 
 class FluidMixture:
     """Easily create mixtures from Fluid objects, e.g. to observe the performance impact of additives."""
 
 class Manifold:
     """The manifold class is used to organise injection elements by their propellant, and provide fluid property attributes to elements."""
-    
