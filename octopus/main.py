0b8960a49321197b4f5b1342b778526d66b45d1c
Henry Free <hf360@cam.ac.uk>
2021-03-23 02:26:41 +0000

    Chemical property source implemented as thermo

--- octopus/main.py
+++ octopus/main.py
@@ -1,14 +1,32 @@
 import thermo
 
-class Fluid:
-    """The fluid class is used to implement any fluids passing through the injector."""
 
-    def __init__(self, name):
+class Fluid:
+    """The fluid class is used to store fluid properties in the injector.
+       This class is pretty useless at the moment, as thermo is the only
+       option for properties. If more modules / sources are added,
+       methods will be implemented to standardise attributes.
+
+        Attributes:
+            name (string): Name of a chemical recognised by the source.
+            source (string): Property source. Currently, only option is thermo.
+                             Defaults to "thermo".
+        """
+
+    def __init__(self, name, source="thermo"):
         self.name = name
-        self.chem = thermo.chemical.Chemical(self.name)
+
+        if source == "thermo":
+            self.chem = thermo.chemical.Chemical(self.name)
+        else:
+            raise ValueError("Only \"thermo\" is supported currently.")
+
 
 class FluidMixture:
-    """Easily create mixtures from Fluid objects, e.g. to observe the performance impact of additives."""
+    """Easily create mixtures from Fluid objects, e.g. to observe
+       the performance impact of additives."""
+
 
 class Manifold:
-    """The manifold class is used to organise injection elements by their propellant, and provide fluid property attributes to elements."""
+    """The manifold class is used to organise injection elements by
+       their propellant, and provide fluid property attributes to elements."""
