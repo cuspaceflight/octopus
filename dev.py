eea64d8b18447618ea8d65ee97a2a12d3010da6c
Ellis Collins <ec765@cam.ac.uk>
2021-03-26 14:03:24 +0000

    weird behaviour in the first few lines of dev with previous calculations affecting results, take a look.

--- dev.py
+++ dev.py
@@ -1,20 +1,49 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import thermo
 
 import octopus as octo
 
 
 def main():
-    # p_COM = 15e6
-    # nitrous_saturated = thermo.chemical.Chemical('nitrous oxide', P=1902e3, T=255)
+    p_COM = 15e6
+
+
+    fig, ax = plt.subplots()
+
+    # using T_0=70
+    T = np.linspace(70, 300, 1000)
+    Cp = np.zeros_like(T)
+    for i, t in enumerate(T):
+        nitrous_saturated = thermo.chemical.Chemical('nitrous oxide', T=t)
+
+        nitrous_saturated.P = nitrous_saturated.Psat
+        Cp[i] = nitrous_saturated.Cpl
+    ax.plot(T, Cp, label='$T_0$=70K')
+    # using T_0 = 60
+    T = np.linspace(60, 300, 1000)
+    Cp = np.zeros_like(T)
+    for i, t in enumerate(T):
+        # note a new object is created every loop, no modification
+        nitrous_saturated = thermo.chemical.Chemical('nitrous oxide', T=t)
+
+        nitrous_saturated.P = nitrous_saturated.Psat
+        Cp[i] = nitrous_saturated.Cpl
+    ax.plot(T, Cp, label='$T_0$=60K')
+
+    ax.set_xlabel('T')
+    ax.set_ylabel('Cpl')
+    plt.legend()
+    plt.show()
     # orifice = octo.Orifice(nitrous_saturated, octo.STRAIGHT, 1, 1)
     # print(orifice.C_fo,orifice.T_o,orifice.p_o,orifice.v_fo,orifice.v_fgo,orifice.h_fgo)
     # print(f'omega: {orifice.omega}, v: {orifice.v(0.85*1902e3)}')
 
-    # h1 = octo.saturation_h(octo.NITROUS_OXIDE, 270, 'l')
+    h1 = octo.saturation_h(octo.NITROUS_OXIDE, 270, 'l')
     # print(f'specific enthalpy at inlet: {h1}, should be: -311')
 
     # For testing WIP code
-
+    '''
     fuel_name = "isopropanol"
     oxidiser_name = "nitrous oxide"
 
@@ -42,6 +71,7 @@ def main():
 
     print(f'densities: IPA:{fuel_man.rho}, 90% IPA:{fuel_mix_man.rho}, nitrous oxide:{oxidiser_man.rho}')
     print(f'heat capacities: IPA:{fuel_man.Cp}, 90% IPA:{fuel_mix_man.Cp}, nitrous oxide:{oxidiser_man.Cp}')
+    '''
 
 
 if __name__ == "__main__":
