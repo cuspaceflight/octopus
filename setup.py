2afd9a029d90751fd2f7ced9a05f335c5feb67d3
Ellis Collins <ec765@cam.ac.uk>
2021-04-04 17:24:03 +0100

    fixed datafile path

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='octopus',
-    version='0.1-beta',
+    version='0.1',
     packages=['octopus'],
     install_requires=['numpy', 'scipy', 'thermo'],
-    extra_objects=[('Lib/site-packages/octopus/data', ['octopus/10024-97-2.json'])],
+    data_files=[('lib/site-packages/octopus/data', ['octopus/10024-97-2.json'])],
     license='GNU General Public License v3.0',
     author_email='ec765@cam.ac.uk',
     description='Utility for analysis of 2-phase compressible flow through an injector'
