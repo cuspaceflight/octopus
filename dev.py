8a211d75019a556d26cd29eb55b85c116fc664df
Ellis Collins <ec765@cam.ac.uk>
2021-04-04 03:15:24 +0100

    Finished adding doctrings
    To do:
    *   Add reference papers
    *   Add derivative notation
    *   Add general annotation

--- dev.py
+++ dev.py
@@ -1,14 +1,14 @@
 import time
 
 from matplotlib import pyplot as plt
-from numpy import linspace, seterr
+from numpy import linspace
 
-from octopus import Fluid, Orifice, STRAIGHT
+from octopus import Fluid, Orifice
 
 
 def main():
     fluid = Fluid('N2O', P=18e5)
-    orifice = Orifice(fluid, 0, STRAIGHT, 1e-2, 1e-3)
+    orifice = Orifice(fluid, 1e-2, 1e-3)
 
     P_cc = linspace(0, 19e5, 50)
     SPI = []
@@ -20,7 +20,7 @@ def main():
         HEM.append(orifice.m_dot_HEM(Pcc))
         DYER.append(orifice.m_dot_dyer(Pcc))
     dt = time.time() - t0
-    print(dt)
+    print(dt, len(P_cc))
 
     plt.plot(P_cc, SPI, label='SPI')
     plt.plot(P_cc, HEM, label='HEM')
--- octopus/main.py
+++ octopus/main.py
@@ -7,6 +7,7 @@ References:
 from functools import lru_cache
 from json import load
 from os.path import dirname
+from typing import List, Dict, Sequence
 
 from numpy import pi, sqrt, array, log, exp, nan_to_num, inf
 from scipy.optimize import least_squares
@@ -18,9 +19,15 @@ cwd = dirname(__file__)
 
 
 class Fluid(chemical.Chemical):
-    """Inherits the thermo Chemical class, and represents a fluid with some overriden properties"""
+    """Inherits the thermo Chemical class, and represents a fluid with a Helmholz EOS."""
 
     def __init__(self, ID: str, T: float = 298.15, P: float = 101325):
+        """Initiate a Fluid instance.
+
+        :param ID: id of fluid with a name recognisable by thermo
+        :param T: Temperature (K)
+        :param P: Pressure (Pa)
+        """
         super().__init__(ID, T, P)
 
         with open(f'{cwd}\\{self.ID}.json', 'r') as f:
@@ -36,9 +43,14 @@ class Fluid(chemical.Chemical):
         self.polar = bool(data['polar'])
 
     @lru_cache(maxsize=1)
-    def alpha_0(self, delta, tau):
-        """Calculate the reduced ideal gas Helmholz energy is the fluid coefficients are known"""
+    def alpha_0(self, delta: float, tau: float):
+        """Calculate the reduced ideal gas Helmholz energy.
 
+        :param delta: rho/rho_c to be evaluated at
+        :param tau: T_c/T to be evaluated at
+        :return: ideal gas component of Helmholz energy
+        :rtype: float
+        """
         alpha_0 = (self.a[0]
                    + self.a[1] * tau
                    + log(delta)
@@ -48,9 +60,13 @@ class Fluid(chemical.Chemical):
         return alpha_0
 
     @lru_cache(maxsize=1)
-    def alpha_r(self, delta, tau):
-        """calculates the reduced helmholz residual energy if the chemical is nitrus oxide"""
+    def alpha_r(self, delta: float, tau: float) -> float:
+        """Calculate the reduced helmholz residual energy.
 
+        :param delta: rho/rho_c to be evaluated at
+        :param tau: T_c/T to be evaluated at
+        :return: residual component of Helmholz energy
+        """
         if self.polar:
 
             alpha_r = (self.n[0] * delta * tau ** 0.25
@@ -69,94 +85,209 @@ class Fluid(chemical.Chemical):
         else:
             raise NotImplementedError
 
-    def rho_g(self, T):
+    def rho_g(self, T: float) -> float:
+        """Evaluate vapour density at T.
+
+        :param T: Temperature (K)
+        :return: vapour density
+        """
         tau = 1 - T / self.Tc
         return nan_to_num(self.rhoc + self.Ag[0] * (tau ** 0.35 - 1) + sum(a * tau ** i for i, a in enumerate(self.Ag)))
 
-    def rho_l(self, T):
+    def rho_l(self, T: float) -> float:
+        """Evaluate liquid density at T.
+
+        :param T: Temperature (K)
+        :return: liquid density
+        """
         tau = 1 - T / self.Tc
         return nan_to_num(self.rhoc + self.Al[0] * (tau ** 0.35 - 1) + sum(a * tau ** i for i, a in enumerate(self.Al)))
 
     @lru_cache(maxsize=1)
-    def a0_t(self, delta, tau):
+    def a0_t(self, delta: float, tau: float) -> float:
+        """Evaluate derivative of alpha_0 with respect to tau.
+
+        :param delta: rho/rho_c to be evaluated at
+        :param tau: T_c/T to be evaluated at
+        :return: d(alpha_0)/d(tau)
+        """
         return derivative(self.alpha_0, 1, delta, tau)
 
     @lru_cache(maxsize=1)
-    def a0_tt(self, delta, tau):
+    def a0_tt(self, delta: float, tau: float) -> float:
+        """Evaluate second derivative of alpha_0 with respect to tau.
+
+        :param delta: rho/rho_c to be evaluated at
+        :param tau: T_c/T to be evaluated at
+        :return: d2(alpha_0)/d(tau)d(tau)
+        """
         return derivative(self.a0_t, 1, delta, tau)
 
     @lru_cache(maxsize=1)
-    def ar_d(self, delta, tau):
+    def ar_d(self, delta: float, tau: float) -> float:
+        """Evaluate derivative of alpha_r with respect to delta.
+
+        :param delta: rho/rho_c to be evaluated at
+        :param tau: T_c/T to be evaluated at
+        :return: d(alpha_r)/d(delta)
+        """
         return derivative(self.alpha_r, 0, delta, tau)
 
     @lru_cache(maxsize=1)
-    def ar_t(self, delta, tau):
+    def ar_t(self, delta: float, tau: float) -> float:
+        """Evaluate derivative of alpha_r with respect to tau.
+
+        :param delta: rho/rho_c to be evaluated at
+        :param tau: T_c/T to be evaluated at
+        :return: d(alpha_r)/d(tau)
+        """
         return derivative(self.alpha_r, 1, delta, tau)
 
     @lru_cache(maxsize=1)
-    def ar_dd(self, delta, tau):
+    def ar_dd(self, delta: float, tau: float) -> float:
+        """Evaluate second derivative of alpha_r with respect to delta.
+
+        :param delta: rho/rho_c to be evaluated at
+        :param tau: T_c/T to be evaluated at
+        :return: d2(alpha_r)/d(delta)d(delta
+        """
         return derivative(self.ar_d, 0, delta, tau)
 
     @lru_cache(maxsize=1)
-    def ar_dt(self, delta, tau):
+    def ar_dt(self, delta: float, tau: float) -> float:
+        """Evaluate derivative of alpha_r with respect to delta and tau.
+
+        :param delta: rho/rho_c to be evaluated at
+        :param tau: T_c/T to be evaluated at
+        :return: d2(alpha_r)/d(delta)d(tau)
+        """
         return derivative(self.ar_d, 1, delta, tau)
 
     @lru_cache(maxsize=1)
-    def ar_tt(self, delta, tau):
+    def ar_tt(self, delta: float, tau: float) -> float:
+        """Evaluate second derivative of alpha_r with respect to tau.
+
+        :param delta: rho/rho_c to be evaluated at
+        :param tau: T_c/T to be evaluated at
+        :return: d2(alpha_r)/d(tau)d(tau)
+        """
         return derivative(self.ar_t, 1, delta, tau)
 
     @lru_cache(maxsize=1)
-    def dp_drho(self, rho, T):
+    def dp_drho(self, rho: float, T: float) -> float:
+        """Evaluate derviative of pressure with respect to density.
+
+        :param rho: density to be evaluated at
+        :param T: temperature to be evaluated at
+        :return: d(p)/d(rho)
+        """
         delta = rho / self.rhoc
         tau = self.Tc / T
         return self.R_specific * T * (1 + 2 * delta * self.ar_d(delta, tau) + delta * delta * self.ar_dd(delta, tau))
 
     @lru_cache(maxsize=1)
-    def dg_drho(self, rho, T):
+    def dg_drho(self, rho: float, T: float) -> float:
+        """Evaluate derviative of specific gibbs free energy with respect to density.
+
+        :param rho: density to be evaluated at
+        :param T: temperature to be evaluated at
+        :return: d(g)/d(rho)
+        """
         delta = rho / self.rhoc
         tau = self.Tc / T
         return self.R_specific * T * (2 * self.ar_d(delta, tau) + delta * self.ar_dd(delta, tau))
 
     @lru_cache(maxsize=1)
-    def p(self, rho, T):
+    def p(self, rho: float, T: float) -> float:
+        """Evaluate pressure.
+
+        :param rho: density
+        :param T: temperature
+        :return: pressure
+        """
         delta = rho / self.rhoc
         tau = self.Tc / T
-        return rho * self.R_specific * T * \
-               (1 + delta * self.ar_d(delta, tau))
+        return (rho * self.R_specific * T
+                * (1 + delta * self.ar_d(delta, tau)))
 
     @lru_cache(maxsize=1)
-    def u(self, rho, T):
+    def u(self, rho: float, T: float) -> float:
+        """Evaluate specific internal energy.
+
+        :param rho: density
+        :param T: temperature
+        :return: specific internal energy
+        """
         delta = rho / self.rhoc
         tau = self.Tc / T
-        return self.R_specific * T * \
-               tau * (self.a0_t(delta, tau) + self.ar_t(delta, tau))
+        return (self.R_specific * T
+                * tau * (self.a0_t(delta, tau) + self.ar_t(delta, tau)))
 
     @lru_cache(maxsize=1)
-    def h(self, rho, T):
+    def h(self, rho: float, T: float) -> float:
+        """Evaluate specific enthalpy.
+
+        :param rho: density
+        :param T: temperature
+        :return: specific enthalpy
+        """
         delta = rho / self.rhoc
         tau = self.Tc / T
-        return self.R_specific * T * \
-               (1 + tau * (self.a0_t(delta, tau) + self.ar_t(delta, tau)) + delta * self.ar_d(delta, tau))
+        return (self.R_specific * T
+                * (1 + tau * (self.a0_t(delta, tau) + self.ar_t(delta, tau)) + delta * self.ar_d(delta, tau)))
 
     @lru_cache(maxsize=1)
-    def s(self, rho, T):
+    def s(self, rho: float, T: float) -> float:
+        """Evaluate specific entropy.
+
+        :param rho: density
+        :param T: temperature
+        :return: specific entropy
+        """
         delta = rho / self.rhoc
         tau = self.Tc / T
-        return self.R_specific * \
-               (tau * (self.a0_t(delta, tau) + self.ar_t(delta, tau)) - self.alpha_0(delta, tau) - self.alpha_r(delta, tau))
+        return (self.R_specific * (
+                tau * (self.a0_t(delta, tau) + self.ar_t(delta, tau)) - self.alpha_0(delta, tau) - self.alpha_r(delta,
+                                                                                                                tau)))
 
     @lru_cache(maxsize=1)
-    def g(self, rho, T):
+    def g(self, rho: float, T: float) -> float:
+        """Evaluate specific gibbs free energy
+
+        :param rho: density
+        :param T: temperature
+        :return: specific gibbs free energy
+        """
         delta = rho / self.rhoc
         tau = self.Tc / T
-        return self.R_specific * T * \
-               (1 + self.alpha_0(delta, tau) + self.alpha_r(delta, tau) + delta * self.ar_d(delta, tau))
+        return (self.R_specific * T
+                * (1 + self.alpha_0(delta, tau) + self.alpha_r(delta, tau) + delta * self.ar_d(delta, tau)))
 
-    def fun_ps(self, x, u, y):
+    def fun_ps(self, x: Sequence[float], u: Sequence[str], y: Sequence[float]) -> List[float]:
+        """Return a vectorised cost function to be used in a property solver
+
+        Usage:
+            fun_ps([rho, T], ['p', 's'], [p0, s0]) --> [p(rho, T) - p0, s(rho, T) - s0]
+
+            where [d(p),d(s)] is the difference between  [p,s] calculated from [rho,T] and [p0,s0]
+            i.e to be used as a non-linear solver's cost function.
+
+
+        :param x: object containing arguments to be passed to get_properties
+        :param u: object containing property names of properties to be returned and compared
+        :param y: object containing property values to compare returned values to
+        :return: object containing difference between calculated property values and those in y
+        """
         return [self.get_properties(x[0], x[1])[var] - val for var, val in zip(u, y)]
 
     @lru_cache(maxsize=1)
-    def get_properties(self, rho, T):
+    def get_properties(self, rho: float, T: float) -> Dict[str, float]:
+        """Return properties in a dict
+
+        :param rho: density
+        :param T: temperature
+        :return: dict where keys are property names referring to float values
+        """
         rhog = self.rho_g(T)
         rhol = self.rho_l(T)
 
@@ -197,10 +328,19 @@ class Manifold:
         self.Cp = self.fluid.Cp
 
 
-class Orifice:  # WIP
+class Orifice:
     """Model the thermodynamic changes as fluid moves through the orifice"""
 
-    def __init__(self, fluid, chi0, orifice_type, L, D, Cd=0.7):
+    def __init__(self, fluid: Fluid, L: float, D: float, chi0: float = 0, orifice_type: int = 0, Cd: float = 0.7):
+        """Initiate an Orifice instance.
+
+        :param fluid: Fluid to use cost funtions and initial properties from
+        :param L: orifice length (m)
+        :param D: orifice diameter (m)
+        :param chi0: initial vapour mass fraction = 0
+        :param orifice_type: only octopus.STRAIGHT = 0 supported
+        :param Cd: discharge coefficient = 0.7
+        """
         # subscript o represents initial conditions at stagnation
         self.fluid = fluid
         self.P_o = fluid.P
@@ -213,14 +353,20 @@ class Orifice:  # WIP
         self.Cd = Cd
 
     @lru_cache(maxsize=1)
-    def m_dot_SPI(self, P_cc):
+    def m_dot_SPI(self, P_cc: float):
+        """Return single-phase-incompressible mass flow rate
+
+        :param P_cc: combustion chamber pressure (Pa)
+        :return: mass flow rate (kg/s)
+        """
         if self.orifice_type == 0:
             # find initial conditions
             # chi0,p0 known
             p0 = self.P_o
             chi0 = self.chi0
 
-            if p0 < P_cc: return None
+            if p0 < P_cc:
+                return None
 
             u = ['p', 'chi']
             y = [p0, chi0]
@@ -231,14 +377,20 @@ class Orifice:  # WIP
             return self.A * sqrt(2 * rho0 * (self.P_o - P_cc))
 
     @lru_cache(maxsize=1)
-    def m_dot_HEM(self, P_cc):
+    def m_dot_HEM(self, P_cc: float):
+        """Return homogeneous-equilibrium-model mass flow rate
+
+        :param P_cc: combustion chamber pressure (Pa)
+        :return: mass flow rate (kg/s)
+        """
         if self.orifice_type == 0:
             # find initial conditions
             # chi0,p0 known
             p0 = self.P_o
             chi0 = self.chi0
 
-            if p0 < P_cc: return None
+            if p0 < P_cc:
+                return None
 
             u = ['p', 'chi']
             y = [p0, chi0]
@@ -261,13 +413,19 @@ class Orifice:  # WIP
             rho1, T1 = final.x
 
             h1 = self.fluid.get_properties(rho1, T1)['h']
-            X1 = self.fluid.get_properties(rho1, T1)['chi']
-            if h1 > h0: return None
+            if h1 > h0:
+                return None
             return self.A * rho1 * sqrt(h0 - h1)
 
     @lru_cache(maxsize=1)
-    def m_dot_dyer(self, P_cc):
+    def m_dot_dyer(self, P_cc: float):
+        """Return Dyer model mass flow rate
+
+        :param P_cc: combustion chamber pressure (Pa)
+        :return: mass flow rate (kg/s)
+        """
         kappa = 1  # fluid enters orifice at vapour pressure
         W = 1 / (1 + kappa)
-        if not (self.m_dot_SPI(P_cc) and self.m_dot_HEM(P_cc)): return None
+        if not (self.m_dot_SPI(P_cc) and self.m_dot_HEM(P_cc)):
+            return None
         return self.Cd * ((1 - W) * self.m_dot_SPI(P_cc) + W * self.m_dot_HEM(P_cc))
