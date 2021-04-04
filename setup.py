cb4c7f8ac9a47b7ea9bbcad9d74ea84b68290cd6
Ellis Collins <ec765@cam.ac.uk>
2021-04-04 17:04:16 +0100

    change datafile save path

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1-beta',
     packages=find_packages(),
     install_requires=['numpy', 'scipy', 'thermo'],
-    data_files=[('data', ['octopus/10024-97-2.json'])],
+    data_files=[('Lib/site-packages/octopus/data', ['octopus/10024-97-2.json'])],
     license='GNU General Public License v3.0',
     author_email='ec765@cam.ac.uk',
     description='Utility for analysis of 2-phase compressible flow through an injector'
