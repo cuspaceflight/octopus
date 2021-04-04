5ac22b2b6e16cc4c4e62ab59cc18320c0082fec0
Ellis Collins <ec765@cam.ac.uk>
2021-04-04 16:37:31 +0100

    added option to change EOS method

--- examples/dev.py
+++ examples/dev.py
@@ -14,23 +14,23 @@ return 2.49973 * (1 + 0.023454 / (1 - Tr) - 3.80136 * (1 - Tr) +
 
 
 def main():
-    fluid = Fluid('nitrous oxide', P=18e5)
-    orifice = Orifice(fluid, 1e-2, 1e-3)
+    nitrous = Fluid('nitrous oxide', P=18e5, method='helmholz')
+    isopropanol = Fluid('isopropanol', P=18e5, method='thermo')
+    nitrous_orifice = Orifice(nitrous, 1e-2, 1e-3)
+    ipa_orifice = Orifice(isopropanol, 1e-2, 1e-3)
 
     P_cc = linspace(0, 19e5, 50)
     SPI = []
-    HEM = []
     DYER = []
     for Pcc in P_cc:
-        SPI.append(orifice.m_dot_SPI(Pcc))
-        HEM.append(orifice.m_dot_HEM(Pcc))
-        DYER.append(orifice.m_dot_dyer(Pcc))
+        SPI.append(ipa_orifice.m_dot_SPI(Pcc))
+        DYER.append(nitrous_orifice.m_dot_dyer(Pcc))
 
-    plt.plot(P_cc, SPI, label='SPI')
-    plt.plot(P_cc, HEM, label='HEM')
-    plt.plot(P_cc, DYER, label='DYER')
+    plt.plot(P_cc, SPI, label='IPA')
+    plt.plot(P_cc, DYER, label='N2O')
     plt.xlabel('Downstream pressure (Pa)')
     plt.ylabel('Mass Flow Rate (kg/s)')
+    plt.legend()
     plt.show()
 
 
