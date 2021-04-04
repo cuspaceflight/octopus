82c5a0fecd0c656ac44a3b166e01476377eb6dbe
Ellis Collins <ec765@cam.ac.uk>
2021-04-04 17:32:33 +0100

    made setup.py nicer

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     packages=['octopus'],
     install_requires=['numpy', 'scipy', 'thermo'],
-    data_files=[('lib/site-packages/octopus/data', ['octopus/10024-97-2.json'])],
+    package_data={'octopus': ['data/*.json']},
     license='GNU General Public License v3.0',
     author_email='ec765@cam.ac.uk',
     description='Utility for analysis of 2-phase compressible flow through an injector'
