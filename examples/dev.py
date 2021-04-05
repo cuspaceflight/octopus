73587efe688fe86401c2d9244fd453d7562223f6
Ellis Collins <ec765@cam.ac.uk>
2021-04-06 00:21:58 +0100

    moved files to /examples

--- examples/dev.py
+++ examples/dev.py
@@ -1,5 +1,6 @@
 import numpy as np
 from octopus import Fluid, Orifice
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -19,13 +20,19 @@ def main():
 
     P_cc = np.linspace(0, 19e5, 50)
     SPI = []
+    SPIn=[]
+    HEM=[]
     DYER = []
     for Pcc in P_cc:
         SPI.append(ipa_orifice.m_dot_SPI(Pcc))
+        SPIn.append(nitrous_orifice.m_dot_SPI(Pcc))
+        HEM.append(nitrous_orifice.m_dot_HEM(Pcc))
         DYER.append(nitrous_orifice.m_dot_dyer(Pcc))
 
     plt.plot(P_cc, SPI, label='IPA')
-    plt.plot(P_cc, DYER, label='N2O')
+    plt.plot(P_cc, SPIn, label='SPI nitrous')
+    plt.plot(P_cc, HEM, label='HEM nitrous')
+    plt.plot(P_cc, DYER, label='DYER nitrous')
     plt.xlabel('Downstream pressure (Pa)')
     plt.ylabel('Mass Flow Rate (kg/s)')
     plt.legend()
