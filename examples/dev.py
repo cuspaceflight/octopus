7ebf8a783fea2b5e8cf4fc5490d1f919a64e5eaa
Ellis Collins <ec765@cam.ac.uk>
2021-04-05 15:56:17 +0100

    moved dev to the /examples directory

--- examples/dev.py
+++ examples/dev.py
@@ -3,21 +3,18 @@ from numpy import linspace
 
 from octopus import Fluid, Orifice
 
-"""LEGACY CPL CODE
-
-if not 183.15 <= T <= 303.15:
-    raise ValueError(f"Temperature ({T} K) out of range")
-Tr = 309.57  # Find the reduced temperature (T / T_crit)
-return 2.49973 * (1 + 0.023454 / (1 - Tr) - 3.80136 * (1 - Tr) +
-                  13.0945 * (1 - Tr) ** 2 - 14.5180 * (1 - Tr) ** 3)
-"""
-
-
 def main():
     nitrous = Fluid('nitrous oxide', P=18e5, method='helmholz')
     isopropanol = Fluid('isopropanol', P=18e5, method='thermo')
     nitrous_orifice = Orifice(nitrous, 1e-2, 1e-3)
     ipa_orifice = Orifice(isopropanol, 1e-2, 1e-3)
+
+    # Used to check against the injector Waxman built
+    fluid2 = Fluid('nitrous oxide', P=48.4e5, T=256, method="helmholz")
+    check_orifice = Orifice(fluid2, 1e-2, 2.15e-3, orifice_type=1)
+
+    [print(f'{key}: {val}') for key, val in check_orifice.m_dot_waxman(37.1e5).items()]
+    print(f'D2: {check_orifice.D}')
 
     P_cc = linspace(0, 19e5, 50)
     SPI = []
