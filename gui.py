70849355047ee3e6b8332f050f5609bc57f16d91
Henry Free <hf360@cam.ac.uk>
2021-04-15 23:37:43 +0100

    tkinter testing

--- gui.py
+++ gui.py
@@ -1,2 +1,12 @@
 from octopus import Fluid, Orifice, PropertySource, Manifold, Element
 import tkinter as tk
+
+window_cfg = tk.Tk()
+window_cfg.title("Octopus - injector face pattern analysis")
+frame_top = tk.Frame(master=window_cfg, width=200, height=5, padx=5, pady=5)
+frame_top.grid(row=0, column=0)
+
+title = tk.Label(master=frame_top, text="Octopus - injector face pattern analysis", height=1)
+title.pack()
+
+window_cfg.mainloop()
