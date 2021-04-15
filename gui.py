6338adf8724184e2afab2046494278bc831a7788
Henry Free <hf360@cam.ac.uk>
2021-04-15 23:40:41 +0100

    New window for displaying plate configuration

--- gui.py
+++ gui.py
@@ -1,11 +1,20 @@
 from octopus import Fluid, Orifice, PropertySource, Manifold, Element
 import tkinter as tk
 
+    plate_window = tk.Tk()
+    plate_window.columnconfigure(0, weight=1)
+    plate_window.rowconfigure([0, 1], weight=1)
+    plate_top_frame = tk.Frame(master=plate_window, height=30, padx=5, pady=5)
+    plate_top_frame.grid(row=0, column=0)
+    plate_title = tk.Label(master=plate_top_frame, text="Injector plate configuration")
+    plate_title.pack()
+    plate_window.mainloop()
 window_cfg = tk.Tk()
 window_cfg.iconphoto(True, tk.PhotoImage(file="img/favicon.png"))
 window_cfg.title("Octopus - injector face pattern analysis")
 window_cfg.columnconfigure(0, weight=1)
 window_cfg.rowconfigure([0, 1], weight=1)
+
 frame_top = tk.Frame(master=window_cfg, width=200, height=5, padx=5, pady=5)
 frame_top.grid(row=0, column=0)
 
@@ -20,4 +29,8 @@ diameter_entry.grid(row=0, column=1, sticky="w")
 
 diameter_entry_label = tk.Label(master=frame_bottom, text="plate diameter, mm")
 diameter_entry.grid(row=0, column=1, sticky="e")
+
+new_plate_button = tk.Button(master=frame_bottom, text="Create injector plate", command=new_plate)
+new_plate_button.grid(row=1, column=0, sticky="s")
+
 window_cfg.mainloop()
